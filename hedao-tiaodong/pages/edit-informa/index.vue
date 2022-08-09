efe136e093eb3fc4e09bb647cf24b20a747b2b49
mayingyi <mayy@77ircloud.com>
2022-08-09 09:06:17 +0800

    feat: 新增编辑资料页

---
+++ hedao-tiaodong/pages/edit-informa/index.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="content">
+		<div class="flex align-center register-box">姓名
+            <span class="register-box-txt">
+			    你的昵称 >
+            </span>
+        </div>
+		<div class="flex align-center register-box">邮箱
+			<span class="register-box-txt">
+			    你的收货邮箱 >
+            </span>
+		</div>
+		<div class="flex align-center register-box">地址
+			<span class="register-box-txt">
+			    实物收货地址 >
+            </span>
+		</div>
+	</view>
+</template>
+
+<script>
+	export default {
+		components: {
+		},
+		data() {
+			return {
+				nikename: '',
+				email: '',
+				address: '',
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			navigateTo() {
+				uni.navigateTo({
+					url: '../../pages/question/index'
+				})
+			},
+		}
+	}
+</script>
+
+<style>
+	@import url("./index.css");
+</style>
