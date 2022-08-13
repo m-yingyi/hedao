aa43a9afbedcc1b51aafd40b16668e57e321e6e2
mayingyi <mayy@77ircloud.com>
2022-08-13 20:15:02 +0800

    feat: 开通会员点击交互

--- hedao-tiaodong/pages/member-payment/index.vue
+++ hedao-tiaodong/pages/member-payment/index.vue
@@ -30,12 +30,12 @@
 		<div class="support-way" style="margin-top: 20upx;">
 			<h2 style="font-size: 32upx; line-height: 32upx;font-weight:bold;padding-bottom: 20upx;">选择会员期限</h2>
 			<ul class="way-box longTime">
-				<li data-price="300" data-timetype="1"><p><image src="/static/yun/imgs/png_201_2.png" mode="widthFix"/>一个月</p><image src="/static/yun/imgs/icon_yun_938.png"/></li>
-				<li data-price="900" data-timetype="2"><p><image src="/static/yun/imgs/png_201_2.png" mode="widthFix"/>三个月</p><image src="/static/yun/imgs/icon_yun_938_2.png"/></li>
-				<li data-price="1800" data-timetype="3"><p><image src="/static/yun/imgs/png_201_2.png" mode="widthFix"/>半年</p><image src="/static/yun/imgs/icon_yun_938_2.png"/></li>
-				<li data-price="3420" data-original="3600" data-timetype="4"><p><image src="/static/yun/imgs/png_201_2.png" mode="widthFix" />一年<span>95%折扣</span></p><image src="/static/yun/imgs/icon_yun_938_2.png"/></li>
-				<li data-price="6480" data-original="7200" data-timetype="5"><p><image src="/static/yun/imgs/png_201_2.png" mode="widthFix" />二年<span>90%折扣</span></p><image src="/static/yun/imgs/icon_yun_938_2.png"/></li>
-				<li data-price="9180" data-original="10800" data-timetype="6"><p><image src="/static/yun/imgs/png_201_2.png" mode="widthFix" />三年<span>85%折扣</span></p><image src="/static/yun/imgs/icon_yun_938_2.png"/></li>
+				<li data-price="300" data-timetype="1" @click="clickPlan($event)" ><p><image src="/static/yun/imgs/png_201_2.png" mode="widthFix"/>一个月</p><image :src="`/static/yun/imgs/icon_yun_938${selectedPlan == 1 ? '' : '_2'}.png`"/></li>
+				<li data-price="900" data-timetype="2" @click="clickPlan($event)"><p><image src="/static/yun/imgs/png_201_2.png" mode="widthFix"/>三个月</p><image :src="`/static/yun/imgs/icon_yun_938${selectedPlan == 2 ? '' : '_2'}.png`"/></li>
+				<li data-price="1800" data-timetype="3" @click="clickPlan($event)"><p><image src="/static/yun/imgs/png_201_2.png" mode="widthFix"/>半年</p><image :src="`/static/yun/imgs/icon_yun_938${selectedPlan == 3 ? '' : '_2'}.png`"/></li>
+				<li data-price="3420" data-original="3600" data-timetype="4" @click="clickPlan($event)"><p><image src="/static/yun/imgs/png_201_2.png" mode="widthFix" />一年<span>95%折扣</span></p><image :src="`/static/yun/imgs/icon_yun_938${selectedPlan == 4 ? '' : '_2'}.png`"/></li>
+				<li data-price="6480" data-original="7200" data-timetype="5" @click="clickPlan($event)"><p><image src="/static/yun/imgs/png_201_2.png" mode="widthFix" />二年<span>90%折扣</span></p><image :src="`/static/yun/imgs/icon_yun_938${selectedPlan == 5 ? '' : '_2'}.png`"/></li>
+				<li data-price="9180" data-original="10800" data-timetype="6" @click="clickPlan($event)"><p><image src="/static/yun/imgs/png_201_2.png" mode="widthFix" />三年<span>85%折扣</span></p><image :src="`/static/yun/imgs/icon_yun_938${selectedPlan == 6 ? '' : '_2'}.png`"/></li>
 			</ul>
 		</div>
 		<div style="height: 88upx;width: 100%;"></div>
@@ -53,22 +53,48 @@
 
 <script>
 import BaseSpace from '@/components/BaseSpace.vue';
+import Request from '@/common/require.js';
+import API from '@/common/api.js';
 	export default {
 		components: {
 			BaseSpace,
 		},
 		data() {
 			return {
+				id: null,
+				selectedPlan: 1
 			}
 		},
-		onLoad() {
-
+		onLoad(option) {
+            console.log("🚀 ~ file: index.vue ~ line 66 ~ onLoad ~ option", option)
+			this.id = option.id
+			this.getPlan()
 		},
 		methods: {
 			navigateTo() {
 				uni.navigateTo({
 					url: '../../pages/question/index'
 				})
+			},
+			clickPlan(e) {
+                console.log("🚀 ~ file: index.vue ~ line 77 ~ clickPlan ~ e", e)
+				this.selectedPlan = e.target.dataset.timetype;
+			},
+			getPlan() {
+				if(!this.id) {
+					uni.showToast({
+							title: '没有获取到ID',
+							icon: 'none',
+							duration: 3000,
+						})
+				} else {
+					// 接口请求
+				Request.get(API.member.memberPlan + this.id, null, ({statusCode, data}) => {
+                    console.log("🚀 ~ file: index.vue ~ line 92 ~ Request.get ~ data", data)
+					if(statusCode!=200) return;
+					
+				})
+				}
 			}
 		}
 	}
