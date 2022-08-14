89a4b1ab4ed5eb5ed47d44f3273f07667756ad08
mayingyi <mayy@77ircloud.com>
2022-08-14 22:09:59 +0800

    chore: 更换接口名

--- hedao-tiaodong/common/api.js
+++ hedao-tiaodong/common/api.js
@@ -32,7 +32,7 @@ export default {
     },
     member: {
         memberList: '/member/member-list', // 会员列表
-        memberPlan: '/member/member-plan-by-plan-id/', // {palnid} 通过方案ID获取会员方案
+        memberPlan: '/member/member-plan/', // {palnid} 通过方案ID获取会员方案
     },
     information: {
         list: '/information/information-list'
