321e1975e5a3ea997f972c19b795665c8c179cd5
mayingyi <mayy@77ircloud.com>
2022-08-21 22:07:25 +0800

    feat: 新增编辑地址交互

--- hedao-tiaodong/pages/edit-address/index.vue
+++ hedao-tiaodong/pages/edit-address/index.vue
@@ -1,28 +1,29 @@
 <template>
 	<view class="content">
-		<view class="address-box">
+		<view class="address-box" v-for="(item, index) in addAddressLists" :key="index">
 			<view class="box-title">
 				<!-- 图片 -->
-				<image :src="isCheck ? '/static/yun/imgs/icon_yun_938.png' : '/static/yun/imgs/icon_yun_938_2.png'"/>
-				设为默认地址
+				<image @click="clickDefault(index)" :src="item.isDefault ? '/static/yun/imgs/icon_yun_938.png' : '/static/yun/imgs/icon_yun_938_2.png'"/>
+				<span class="decs">设为默认地址</span>
+				<span v-if="index" @click="delAddress(index)" class="del-btn">删除</span>
 			</view>
 			<view class="box-content">
 				<view class="content-item item-name">
 					<text>收货姓名</text>
-					<input type="text" placeholder="你的收货姓名" @input="nameInput" />
+					<input type="text" placeholder="你的收货姓名" @input="(e) => {nameInput(e, index)}" />
 				</view>
 				<view class="content-item item-phone">
 					<text>联系方式</text>
-					<input type="number" placeholder="你的联系方式" @input="phoneInput" />
+					<input type="number" placeholder="你的联系方式" @input="(e) => {phoneInput(e, index)}" />
 				</view>
 				<view class="content-item item-address">
 					<text>地址</text>
-					<input type="text" placeholder="你的详细地址" @input="addressInput" />
+					<input type="text" placeholder="你的详细地址" @input="(e) => {addressInput(e, index)}" />
 				</view>
 			</view>
 		</view>
-		<div class="address-gray-btn">添加新地址</div>
-		<div class="footer-btn">保存设置</div>
+		<div class="address-gray-btn" @click="add">添加新地址</div>
+		<div class="footer-btn" @click="addAddress">保存设置</div>
 	</view>
 </template>
 
@@ -35,11 +36,14 @@ import API from '@/common/api.js';
 		data() {
 			return {
 				userInfo: {},
-				addressInfo: {
-					name: '',
-					phone: '',
-					address: '',
+				addressCreateInfo: {
+					linkName: '',
+					linkPhone: '',
+					linkAddress: '',
+					isDefault: 0,
+					id: 0,
 				},
+				addAddressLists: [],
 				isCheck: false,
 				addressList: [],
 			}
@@ -47,12 +51,19 @@ import API from '@/common/api.js';
 		onShow() {
 			this.getUserInfo();
 			this.getAddressLists();
+			this.add();
 		},
 		methods: {
 			navigateTo() {
 				uni.navigateTo({
 					url: '../../pages/question/index'
 				})
+			},
+			add() {
+				this.$set(this.addAddressLists, this.addAddressLists.length, {
+					...this.addressCreateInfo,
+					userId: this.userInfo.userId,
+				});
 			},
 			getUserInfo() {
 				// TODO
@@ -65,16 +76,30 @@ import API from '@/common/api.js';
 				});
 			},
 			addAddress() {
-
+				console.log(this.addAddressLists)
+				this.editAddress();
 			},
 			editAddress() {
-				Require.put(API.user.addressEdit, {
-
-				}, ({statusCode, data}) => {
+				Require.put(API.user.addressEdit, this.addAddressLists, ({statusCode, data}) => {
 					if(statusCode!=200) return;
-					this.addressList = data;
 				})
-			}
+			},
+			delAddress(index) {
+				this.addAddressLists.splice(index, 1);
+			},
+			clickDefault(index) {
+				this.addAddressLists.map((item) => item.isDefault = 0);
+				this.addAddressLists[index].isDefault = 1
+			},
+			nameInput(e, index){
+				this.addAddressLists[index].linkName = e.detail.value;
+			},
+			phoneInput(e, index){
+				this.addAddressLists[index].linkPhone = e.detail.value;
+			},
+			addressInput(e, index){
+				this.addAddressLists[index].linkAddress = e.detail.value;
+			},
 		}
 	}
 </script>
