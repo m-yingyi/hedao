46a7303b4653d0bd1d2514e95ec5305e2048c59a
forest-lqc <lqcd-99@qq.com>
2022-08-28 22:54:06 +0800

    feat: 编辑资料

--- hedao-tiaodong/pages/edit-informa/index.vue
+++ hedao-tiaodong/pages/edit-informa/index.vue
@@ -1,67 +1,98 @@
 <template>
-	<view class="content">
-		<div class="flex align-center register-box" @click="goName">姓名
-            <span class="register-box-txt">
-			    {{userInfo.nickName || '你的昵称'}} >
-            </span>
-        </div>
-		<div class="flex align-center register-box" @click="goEmail">邮箱
-			<span class="register-box-txt">
-			    {{userInfo.userEmail ||'你的收货邮箱'}} >
-            </span>
-		</div>
-		<div class="flex align-center register-box" @click="goAddress">地址
-			<span class="register-box-txt">
-			    实物收货地址 >
-            </span>
-		</div>
-	</view>
+  <div id="addr_add_wrap">
+    <div class="project-wrap" v-for="(item, index) in addrList">
+      <div class="addr-wrap" id="spec_div_1">
+        <span :class="item.check?'iconbox iconred' : 'iconbox icongray'" id="spec_state_1"></span
+        ><span>设为默认地址</span>
+        <div class="delete-item" @click="deleteAddr(index)" v-if="addrList.length > 1">删除</div>
+      </div>
+      <div class="inputBox wd-box">
+        <span>收货姓名</span
+        ><input
+          class="i-text"
+          id="spec_name_1"
+          type="text"
+          placeholder="你的收货姓名"
+          maxlength="45"
+		  v-model="item.nameVal"
+        />
+      </div>
+      <div class="inputBox wd-box">
+        <span>联系方式</span
+        ><input
+          class="i-text"
+          id="spec_phone_1"
+          type="text"
+          placeholder="你的联系电话"
+          maxlength="45"
+		  v-model="item.phoneVal"
+        />
+      </div>
+      <div class="_addr_box">
+        <span>地址</span
+        ><textarea
+          class="text-content"
+          id="spec_address_1"
+          placeholder="你的收货地址"
+		  v-model="item.addresVal"
+        ></textarea>
+      </div>
+    </div>
+	<div class="add-addr" @click="createAddr">添加新地址</div>
+	<div class="btnSave">保存资料</div>
+  </div>
 </template>
 
 <script>
-	export default {
-		components: {
-		},
-		data() {
-			return {
-				nikename: '',
-				email: '',
-				address: '',
-				userInfo: {},
-			}
-		},
-		onShow() {
-			this.getUserInfo()
-		},
-		methods: {
-			navigateTo() {
-				uni.navigateTo({
-					url: '../../pages/question/index'
-				})
-			},
-			getUserInfo() {
-				// TODO
-				this.userInfo = uni.getStorageSync('userInfo');
-			},
-			goName() {
-                uni.navigateTo({
-					url: '../../pages/edit-name/index'
-				})
-            },
-            goAddress() {
-                uni.navigateTo({
-					url: '../../pages/edit-address/index'
-				})
-            },
-            goEmail() {
-                uni.navigateTo({
-					url: '../../pages/edit-email/index'
-				})
-            },
-		}
-	}
+export default {
+  components: {},
+  data() {
+    return {
+      nikename: "",
+      email: "",
+      address: "",
+      userInfo: {},
+	  addrList: [{check: true, nameVal: '', phoneVal: '', addresVal: ''}]
+    };
+  },
+  onShow() {
+    this.getUserInfo();
+  },
+  methods: {
+	createAddr() {
+      this.addrList.push({check: false, nameVal: '', phoneVal: '', addresVal: ''})
+    },
+	deleteAddr(index) {
+		this.addrList.splice(index, 1)
+    },
+    navigateTo() {
+      uni.navigateTo({
+        url: "../../pages/question/index",
+      });
+    },
+    getUserInfo() {
+      // TODO
+      this.userInfo = uni.getStorageSync("userInfo");
+    },
+    goName() {
+      uni.navigateTo({
+        url: "../../pages/edit-name/index",
+      });
+    },
+    goAddress() {
+      uni.navigateTo({
+        url: "../../pages/edit-address/index",
+      });
+    },
+    goEmail() {
+      uni.navigateTo({
+        url: "../../pages/edit-email/index",
+      });
+    },
+  },
+};
 </script>
 
 <style>
-	@import url("./index.css");
+@import url("./index.css");
 </style>
